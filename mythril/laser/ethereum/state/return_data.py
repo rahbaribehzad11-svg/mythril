89064ae549e99c3cc314e335bdc8081c5913a878
rahbaribehzad11-svg <rahbaribehzad11@gmail.com>
2026-08-16 16:10:37 +0200

    Track call success separately from return data

--- mythril/laser/ethereum/state/return_data.py
+++ mythril/laser/ethereum/state/return_data.py
@@ -10,13 +10,20 @@ from mythril.laser.smt import (
 class ReturnData:
     """Base returndata class."""
 
-    def __init__(self, return_data: List[BitVec], return_data_size: BitVec) -> None:
+    def __init__(
+        self,
+        return_data: List[BitVec],
+        return_data_size: BitVec,
+        success: bool = True,
+    ) -> None:
         """
 
         :param tx_id:
+        :param success: Whether the call that produced this returndata succeeded.
         """
         self.return_data = return_data
         self.return_data_size = return_data_size
+        self.success = success
 
     @property
     def size(self) -> BitVec:
