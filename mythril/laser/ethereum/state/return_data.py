4e72be834504b4b5476ffae49d3be0a109b090d7
rahbaribehzad11-svg <rahbaribehzad11@gmail.com>
2026-08-16 16:55:12 +0200

    revert incomplete CALL semantic change

--- mythril/laser/ethereum/state/return_data.py
+++ mythril/laser/ethereum/state/return_data.py
@@ -2,18 +2,21 @@
 
 from typing import List
 
-from mythril.laser.smt import BitVec
+from mythril.laser.smt import (
+    BitVec,
+)
 
 
 class ReturnData:
     """Base returndata class."""
 
-    def __init__(
-        self, return_data: List[BitVec], return_data_size: BitVec, success: bool = True
-    ) -> None:
+    def __init__(self, return_data: List[BitVec], return_data_size: BitVec) -> None:
+        """
+
+        :param tx_id:
+        """
         self.return_data = return_data
         self.return_data_size = return_data_size
-        self.success = success
 
     @property
     def size(self) -> BitVec:
